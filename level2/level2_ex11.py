158034dd6c0d3666339766640252b5d6662a9ab2
taesu-park <tsoo1014@gmail.com>
2020-07-21 14:25:06 +0900

    200721 | add programmers

---
+++ level2/level2_ex11.py
@@ -0,0 +1,7 @@
+#(11) 땅따먹기
+
+def solution(land):
+    for i in range(1, len(land)):
+        for j in range(4):
+            land[i][j] += max(land[i-1][:j]+land[i-1][j+1:])
+    return max(land[-1])
