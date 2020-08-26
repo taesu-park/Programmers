aabfa31e080afe78461087f7287aaf98d253aabf
taesu-park <tsoo1014@gmail.com>
2020-08-26 16:06:30 +0900

    200826 | add programmers

---
+++ level2/level2_ex25.py
@@ -0,0 +1,10 @@
+#(25) 가장 큰 정사각형 찾기
+
+def solution(board):
+    width = len(board[0])
+    height = len(board)
+    for x in range(1,height):
+        for y in range(1,width):
+            if board[x][y] == 1:
+                board[x][y] = min(board[x-1][y-1], min(board[x-1][y], board[x][y-1])) + 1
+    return max([item for row in board for item in row])**2
