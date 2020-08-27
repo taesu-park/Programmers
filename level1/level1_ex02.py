1c8007e2843002b87a37657444e54a667f4460f9
taesu-park <tsoo1014@gmail.com>
2020-08-27 13:55:22 +0900

    200827 | edit programmers

--- level1/level1_ex02.py
+++ level1/level1_ex02.py
@@ -1,12 +1,10 @@
-import pprint
+#(2) 크레인 인형뽑기 게임
 
 def solution(board, moves):
     answer = 0
     stack = []
     l = len(board)
     for m in moves:
-        pprint.pprint(board)
-        print(stack)
         for i in range(l):
             if board[i][m-1] != 0:
                 if stack and stack[-1] == board[i][m-1]:
@@ -16,7 +14,4 @@ def solution(board, moves):
                     stack.append(board[i][m-1])
                 board[i][m-1] = 0
                 break
-    return answer
-
-
-solution([[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]],[1, 5, 3, 5, 1, 2, 1, 4])
+    return answer*2
